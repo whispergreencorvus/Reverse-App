efd289a45af40717f374bc103c18aaa1c58185df
Mark Hartman <whispergreencorvus@gmail.com>
2022-02-01 16:30:21 +0100

    tabs to spaces

--- src/com/hartmanmark/reverseapp/Main.java
+++ src/com/hartmanmark/reverseapp/Main.java
@@ -2,11 +2,11 @@ package com.hartmanmark.reverseapp;
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.print("Enter your string: ");
-		Reverse reverse = new Reverse();
-		String[] arrayOfSplitWords = reverse.splittingStringIntoWords();
-		System.out.print("Your reverse string: ");
-		reverse.printReverseWords(arrayOfSplitWords);
-	}
+    public static void main(String[] args) {
+        System.out.print("Enter your string: ");
+        Reverse reverse = new Reverse();
+        String[] arrayOfSplitWords = reverse.splittingStringIntoWords();
+        System.out.print("Your reverse string: ");
+        reverse.printReverseWords(arrayOfSplitWords);
+    }
 }
--- src/com/hartmanmark/reverseapp/Reverse.java
+++ src/com/hartmanmark/reverseapp/Reverse.java
@@ -4,45 +4,45 @@ import java.util.*;
 
 public class Reverse {
 
-	public String scanner() {
-		Scanner newString = new Scanner(System.in);
-		String enteredString = newString.nextLine();
-		newString.close();
-		return enteredString;
-	}
+    public String scanner() {
+        Scanner newString = new Scanner(System.in);
+        String enteredString = newString.nextLine();
+        newString.close();
+        return enteredString;
+    }
 
-	public String[] splittingStringIntoWords() {
-		String stringFromSystemIn = scanner();
-		String[] splitWords = stringFromSystemIn.split(" ");
-		return splitWords;
-	}
+    public String[] splittingStringIntoWords() {
+        String stringFromSystemIn = scanner();
+        String[] splitWords = stringFromSystemIn.split(" ");
+        return splitWords;
+    }
 
-	public void printReverseWords(String[] splitWords) {
-		for (String i : splitWords) {
-			String oneWord = new StringBuilder(i).toString();
-			char[] arrayOfLettersOfWord = oneWord.toCharArray();
-			reverse(arrayOfLettersOfWord);
-			String reverseWord = new String(arrayOfLettersOfWord);
-			String oneReverseWordToPrint = String.join(" ", reverseWord);
-			System.out.print(oneReverseWordToPrint + " ");
-		}
-	}
+    public void printReverseWords(String[] splitWords) {
+        for (String i : splitWords) {
+            String oneWord = new StringBuilder(i).toString();
+            char[] arrayOfLettersOfWord = oneWord.toCharArray();
+            reverse(arrayOfLettersOfWord);
+            String reverseWord = new String(arrayOfLettersOfWord);
+            String oneReverseWordToPrint = String.join(" ", reverseWord);
+            System.out.print(oneReverseWordToPrint + " ");
+        }
+    }
 
-	public void reverse(char str[]) {
-		int r = str.length - 1;
-		int l = 0;
-		while (l < r) {
-			if (!Character.isLetter(str[l])) {
-				l++;
-			} else if (!Character.isLetter(str[r])) {
-				r--;
-			} else {
-				char tmp = str[l];
-				str[l] = str[r];
-				str[r] = tmp;
-				l++;
-				r--;
-			}
-		}
-	}
+    public void reverse(char str[]) {
+        int r = str.length - 1;
+        int l = 0;
+        while (l < r) {
+            if (!Character.isLetter(str[l])) {
+                l++;
+            } else if (!Character.isLetter(str[r])) {
+                r--;
+            } else {
+                char tmp = str[l];
+                str[l] = str[r];
+                str[r] = tmp;
+                l++;
+                r--;
+            }
+        }
+    }
 }
