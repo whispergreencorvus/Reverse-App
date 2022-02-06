6b44e18a1442da266c4071e0335e168b63cf4dad
Mark Hartman <whispergreencorvus@gmail.com>
2022-02-06 17:02:35 +0100

    remove some tests

--- src/com/hartmanmark/reverseapp/Reverse.java
+++ src/com/hartmanmark/reverseapp/Reverse.java
@@ -7,16 +7,16 @@ public class Reverse {
     public String reverseString(String input) {
         String stringFromSystemIn = input;
         String[] arrayOfSplitWords = stringFromSystemIn.split(" ");
-        String stringToPrint = compose(arrayOfSplitWords);
+        String stringToPrint = composeInvertedWords(arrayOfSplitWords);
         return stringToPrint;
     }
 
-    private String compose(String[] arrayOfSplitWords) {
+    private String composeInvertedWords(String[] arrayOfSplitWords) {
         ArrayList<String> outputString = new ArrayList<String>();
         for (String i : arrayOfSplitWords) {
             String oneWord = new StringBuilder(i).toString();
             char[] arrayOfLettersOfWord = oneWord.toCharArray();
-            reverse(arrayOfLettersOfWord);
+            reversingArrayOfLetters(arrayOfLettersOfWord);
             String reverseWord = new String(arrayOfLettersOfWord);
             outputString.add(reverseWord);
         }
@@ -24,20 +24,20 @@ public class Reverse {
         return bindedWords;
     }
 
-    private void reverse(char str[]) {
-        int r = str.length - 1;
-        int l = 0;
-        while (l < r) {
-            if (!Character.isLetter(str[l])) {
-                l++;
-            } else if (!Character.isLetter(str[r])) {
-                r--;
+    private void reversingArrayOfLetters(char str[]) {
+        int i = str.length - 1;
+        int j = 0;
+        while (j < i) {
+            if (!Character.isLetter(str[j])) { 
+                j++; 
+            } else if (!Character.isLetter(str[i])) {
+                i--;
             } else {
-                char tmp = str[l];
-                str[l] = str[r];
-                str[r] = tmp;
-                l++;
-                r--;
+                char tmp = str[j];  
+                str[j] = str[i];   
+                str[i] = tmp; 
+                j++;
+                i--;
             }
         }
     }
--- test/com/hartmanmark/reverseapp/ReverseTest.java
+++ test/com/hartmanmark/reverseapp/ReverseTest.java
@@ -7,60 +7,23 @@ import org.junit.jupiter.api.Test;
 class ReverseTest {
 
     @Test
-    void testLowUpperCase() {
+    void expectedStringCanBeReversed() {
         Reverse reverse = new Reverse();
         assertEquals("FEDCBA fedCBA fedcba", 
                 reverse.reverseString("ABCDEF ABCdef abcdef"));
     }
 
     @Test
-    void testNumbers() {
+    void expectedNumbersStayInTheirPlaces() {
         Reverse reverse = new Reverse();
         assertEquals("FEDCBA123 fedCBA123 fedcba123", 
                 reverse.reverseString("ABCDEF123 ABCdef123 abcdef123"));
     }
 
     @Test
-    void testSymbols() {
+    void expectedSymbolsStayInTheirPlaces() {
         Reverse reverse = new Reverse();
         assertEquals("FEDCBA!@#$%^&*() fedCBA!@#$%^&*() fedcba!@#$%^&*()",
                 reverse.reverseString("ABCDEF!@#$%^&*() ABCdef!@#$%^&*() abcdef!@#$%^&*()"));
-    }
-
-    @Test
-    void testAll() {
-        Reverse reverse = new Reverse();
-        assertEquals("FEDCBA123!@#$%^&*() fedCBA123!@#$%^&*() fedcba123!@#$%^&*()",
-                reverse.reverseString("ABCDEF123!@#$%^&*() ABCdef123!@#$%^&*() abcdef123!@#$%^&*()"));
-    }
-
-    @Test
-    void testLontText() {
-        Reverse reverse = new Reverse();
-        assertEquals(
-                "ehT nepO ecruoS noitinifeD stneserp na ecru-osnepo yhposolihp"
-                        + " dna rehtruf senifed eht smret fo esu,"
-                        + " noitacifidom dna noitubirtsider fo ecru-osnepo erawtfos. "
-                        + "erawtfoS sesnecil tnarg sthgir ot sresu "
-                        + "hcihw dluow esiwrehto eb devreser yb thgirypoc wal ot eht "
-                        + "thgirypoc redloh. lareveS ecru-osnepo " 
-                        + "erawtfos sesnecil evah deifilauq nihtiw "
-                        + "eht seiradnuob fo eht nepO ecruoS noitinifeD. ehT tsom "
-                        + "tnenimorp dna ralupop elpmaxe si eht UNG lareneG cilbuP "
-                        + "esneciL (LPG), hcihw \"swolla eerf noitubirtsid "
-                        + "rednu eht noitidnoc taht rehtruf stnempoleved dna "
-                        + "snoitacilppa era tup rednu eht emas ecnecil\", suht osla eerf.",
-                reverse.reverseString("The Open Source Definition presents an open-source philosophy "
-                        + "and further defines the terms of use, "
-                        + "modification and redistribution of open-source software. "
-                        + "Software licenses grant rights to users "
-                        + "which would otherwise be reserved by copyright law to the "
-                        + "copyright holder. Several open-source"
-                        + " software licenses have qualified within the boundaries of "
-                        + "the Open Source Definition. The most"
-                        + " prominent and popular example is the GNU General Public "
-                        + "License (GPL), which \"allows free distribution"
-                        + " under the condition that further developments and "
-                        + "applications are put under the same licence\", thus also free."));
     }
 }
