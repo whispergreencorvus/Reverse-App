021938e9d8f3e55a8ff62ee6409454d629246dae
Mark Hartman <whispergreencorvus@gmail.com>
2022-02-07 13:21:25 +0100

    removing useless values

--- src/com/hartmanmark/reverseapp/Reverse.java
+++ src/com/hartmanmark/reverseapp/Reverse.java
@@ -5,37 +5,33 @@ import java.util.ArrayList;
 public class Reverse {
 
     public String reverseString(String input) {
-        String stringFromSystemIn = input;
-        String[] arrayOfSplitWords = stringFromSystemIn.split(" ");
-        String stringToPrint = composeInvertedWords(arrayOfSplitWords);
-        return stringToPrint;
+        String[] arrayOfSplitWords = input.split(" ");
+        return composeInvertedWords(arrayOfSplitWords);
     }
 
     private String composeInvertedWords(String[] arrayOfSplitWords) {
         ArrayList<String> outputString = new ArrayList<String>();
         for (String i : arrayOfSplitWords) {
-            String oneWord = new StringBuilder(i).toString();
-            char[] arrayOfLettersOfWord = oneWord.toCharArray();
+            char[] arrayOfLettersOfWord = new StringBuilder(i).toString().toCharArray();
             reversingArrayOfLetters(arrayOfLettersOfWord);
             String reverseWord = new String(arrayOfLettersOfWord);
             outputString.add(reverseWord);
         }
-        String bindedWords = String.join(" ", outputString);
-        return bindedWords;
+        return String.join(" ", outputString);
     }
 
     private void reversingArrayOfLetters(char str[]) {
         int i = str.length - 1;
         int j = 0;
         while (j < i) {
-            if (!Character.isLetter(str[j])) { 
-                j++; 
+            if (!Character.isLetter(str[j])) {
+                j++;
             } else if (!Character.isLetter(str[i])) {
                 i--;
             } else {
-                char tmp = str[j];  
-                str[j] = str[i];   
-                str[i] = tmp; 
+                char tmp = str[j];
+                str[j] = str[i];
+                str[i] = tmp;
                 j++;
                 i--;
             }
